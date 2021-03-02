dfab65b235ca7f2c3203840df7d0ff9621d9f7d9
Oleksandr Hrachov <71585565+OleksandrHrachov@users.noreply.github.com>
2021-03-02 09:48:02 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,75 @@
+window.addEventListener('load', function () {
+
+  let counter = document.querySelector('.counter');
+  let icons = document.querySelector('.icons');
+  let play = document.querySelector('.btn-play-again');
+  // let fields = document.querySelectorAll('.field');
+  let userStep;
+  let compStep;
+  let countUser = 0;
+
+  
+
+
+
+  function choiceUser (e) {
+    let target = e.target;
+    if (target.classList.contains('field')) {
+      userStep = target.dataset.field;
+      // target.classList.add('activ');
+      choiceComp();
+    }
+  }
+
+
+  function choiceComp () {
+    let rand = Math.floor(Math.random() * 3);
+    let compFields = icons.querySelectorAll('.field');
+    compStep = compFields[rand].dataset.field;
+    winner();
+  }
+
+
+  function winner () {
+    let comb = userStep + compStep;
+
+    console.log(comb);
+
+    switch (comb) {
+
+      case 'rr':
+      case 'ss':
+      case 'pp':
+        break;
+
+      case 'rs':
+      case 'sp':
+      case 'pr':
+        countUser++;
+        counter.innerText = countUser;
+        break;
+
+      case 'sr':
+      case 'ps':
+      case 'rp':
+        countUser--;
+        counter.innerText = countUser;
+        break;
+    }
+  }
+
+
+
+  function playAgain () {
+    choiceUser (e);
+    choiceComp ();
+    winner ();
+  }
+
+ 
+
+  play.addEventListener('click', playAgain);
+  icons.addEventListener('click', choiceUser);
+});
+
+
